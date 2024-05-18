bf1d901f363f25ad2f7372f17ccc6619772f305f
ahmedragab9089 <ahmed.ragab9089@gmail.com>
2024-05-18 16:31:02 +0300

    adding test case 4

--- src/test/java/tests/TestCase4LogoutUser.java
+++ src/test/java/tests/TestCase4LogoutUser.java
@@ -1,4 +1,39 @@
 package tests;
 
-public class TestCase4LogoutUser {
+import base.BaseTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class TestCase4LogoutUser extends BaseTest {
+    //This method is called here only to register the user to be able to sign with him as we delete the user after registering him in test case 1
+    @BeforeMethod
+    public void createUserWithoutDeletinHim(){
+        registerUserAndDoNotDeleteHim();
+    }
+    @Test
+    public void logoutUser(){
+        //3. Verify that home page is visible successfully
+        String homeURLColor = homePage.getHomeUrlColor();
+        softAssert.assertEquals(homeURLColor, "color: orange;", "You are not in home page");
+        //4. Click on 'Signup / Login' button
+        LoginPage loginPage = homePage.clickSignUpInURL();
+        //5. Verify 'Login to your account' is visible
+        boolean isLoginToYourAccountDisplayed = loginPage.isLoginToYourAccountDisplayed();
+        softAssert.assertTrue(isLoginToYourAccountDisplayed, "The 'login to your account' is not displayed");
+        //6. Enter correct email address and password
+        loginPage.addLoginEmail("ahmedragab9089@gmail.com");
+        loginPage.addPassword("123456");
+        //7. Click 'login' button
+        loginPage.clickLoginButton();
+        //8. Verify that 'Logged in as username' is visible
+        boolean isloggedInAsUserName = homePage.isLoggedInAsUsernameDisplayed();
+        softAssert.assertTrue(isloggedInAsUserName, "You are not logged in");
+        //9. Click 'Logout' button
+        homePage.clickLogoutUrl();
+        //10. Verify that user is navigated to login page
+        String currentURL = driver.getCurrentUrl();
+        softAssert.assertEquals(currentURL, "https://automationexercise.com/login", "You are not in login page");
+        softAssert.assertAll();
+    }
 }
