a16705f19443a51e23b32eda3c50dce834efbbf7
ahmedragab9089 <ahmed.ragab9089@gmail.com>
2024-05-20 23:34:12 +0300

    test case 10

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -9,6 +10,11 @@ public class HomePage {
     private By homeURL = By.linkText("Home");
     private By loggedInURL = By.cssSelector(".fa.fa-user");
     private By testCasesButton = By.cssSelector(".test_cases_list button");
+    private By footer = By.id("footer");
+    private By subscriptionText = By.cssSelector(".single-widget h2");
+    private By subscriptionEmail = By.id("susbscribe_email");
+    private By subscriptionEmailSendButton = By.id("subscribe");
+    private By successSubscriptionMessage = By.cssSelector("#success-subscribe div");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -50,6 +56,24 @@ public class HomePage {
     public TestCasesPage clickTestCasesButton(){
         driver.findElement(testCasesButton).click();
         return new TestCasesPage(driver);
+    }
+    public void scrollToFooter(){
+        WebElement footerEle = driver.findElement(footer);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        // Scroll down to the footer
+        js.executeScript("arguments[0].scrollIntoView(true);", footerEle);
+    }
+    public boolean isSubscriptionTextDisplayed(){
+        return driver.findElement(subscriptionText).isDisplayed();
+    }
+    public void enterSubscriptionEmail(String email){
+        driver.findElement(subscriptionEmail).sendKeys(email);
+    }
+    public void clickSubscriptionEmailSendButton(){
+        driver.findElement(subscriptionEmailSendButton).click();
+    }
+    public String getSuccessSubscriptionMessage(){
+        return driver.findElement(successSubscriptionMessage).getText();
     }
 
 
---
+++ src/test/java/tests/TestCase10VerifySubscriptionInHomePage.java
@@ -0,0 +1,30 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class TestCase10VerifySubscriptionInHomePage extends BaseTest {
+    @Test
+    public void verifySubscriptionInHomePage(){
+        //3. Verify that home page is visible successfully
+        String homeURLColor = homePage.getHomeUrlColor();
+        softAssert.assertEquals(homeURLColor, "color: orange;",
+                "You are not in home page");
+        //4. Scroll down to footer
+        homePage.scrollToFooter();
+        //5. Verify text 'SUBSCRIPTION'
+        boolean isSubscriptionTextDisplayed = homePage.isSubscriptionTextDisplayed();
+        softAssert.assertTrue(isSubscriptionTextDisplayed,
+                "The sunscription text is not displayed");
+        //6. Enter email address in input and click arrow button
+        homePage.enterSubscriptionEmail("ahmedragab9089@gmail.com");
+        homePage.clickSubscriptionEmailSendButton();
+        //7. Verify success message 'You have been successfully subscribed!' is visible
+        String successSubscriptionMessage = homePage.getSuccessSubscriptionMessage();
+        softAssert.assertEquals(successSubscriptionMessage,
+                "You have been successfully subscribed!",
+                "The success message does not appear");
+        
+        softAssert.assertAll();
+    }
+}
