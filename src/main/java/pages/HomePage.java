ba6e574e0d75b8b792ad9a0dd47dda70b7a775da
ahmedragab9089 <ahmed.ragab9089@gmail.com>
2024-05-20 23:43:18 +0300

    test case 11

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -75,6 +75,10 @@ public class HomePage {
     public String getSuccessSubscriptionMessage(){
         return driver.findElement(successSubscriptionMessage).getText();
     }
+    public CartPage clickCartUrl(){
+        clickLink("Cart");
+        return new CartPage(driver);
+    }
 
 
 }
--- src/test/java/tests/TestCase11VerifySubscriptionInCartPage.java
+++ src/test/java/tests/TestCase11VerifySubscriptionInCartPage.java
@@ -2,24 +2,27 @@ package tests;
 
 import base.BaseTest;
 import org.testng.annotations.Test;
+import pages.CartPage;
 
-public class TestCase10VerifySubscriptionInHomePage extends BaseTest {
+public class TestCase11VerifySubscriptionInCartPage extends BaseTest {
     @Test
-    public void verifySubscriptionInHomePage(){
+    public void verifySubscriptionInCartPage(){
         //3. Verify that home page is visible successfully
         String homeURLColor = homePage.getHomeUrlColor();
         softAssert.assertEquals(homeURLColor, "color: orange;",
                 "You are not in home page");
-        //4. Scroll down to footer
+        //4. Click 'Cart' button
+        CartPage cartPage = homePage.clickCartUrl();
+        //5. Scroll down to footer
         homePage.scrollToFooter();
-        //5. Verify text 'SUBSCRIPTION'
+        //6. Verify text 'SUBSCRIPTION'
         boolean isSubscriptionTextDisplayed = homePage.isSubscriptionTextDisplayed();
         softAssert.assertTrue(isSubscriptionTextDisplayed,
                 "The sunscription text is not displayed");
-        //6. Enter email address in input and click arrow button
+        //7. Enter email address in input and click arrow button
         homePage.enterSubscriptionEmail("ahmedragab9089@gmail.com");
         homePage.clickSubscriptionEmailSendButton();
-        //7. Verify success message 'You have been successfully subscribed!' is visible
+        //8. Verify success message 'You have been successfully subscribed!' is visible
         String successSubscriptionMessage = homePage.getSuccessSubscriptionMessage();
         softAssert.assertEquals(successSubscriptionMessage,
                 "You have been successfully subscribed!",
